889f06794c2981509c1458983ae19f7c6e1c5023
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-08-07 17:54:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import ResponsiveAppBar from './components/AppBar';
 import Home from './components/Home';
-
+import Especialidad from './component/Especialidad';
+import Footer from './components/Footer';
 
 function App() {
   return <>
   <ResponsiveAppBar />
   <Home />
+  <Especialidad />
+  <Footer />
   </>;
 }
 
