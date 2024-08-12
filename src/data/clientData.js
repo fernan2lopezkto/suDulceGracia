cb3f0575b4012a5f2c9e83b2cdaa1adb6dc7e083
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-08-12 18:37:39 -0300

    Create clientData.js

---
+++ src/data/clientData.js
@@ -0,0 +1,32 @@
+
+
+const clientData = {
+  companyName: "Nombre de la Empresa",
+  address: "Dirección de la Empresa",
+  phone: "Número de Teléfono",
+  email: "Correo Electrónico",
+  website: "Sitio Web",
+  description: "Breve descripción de la empresa",
+  services: [
+    "Servicio 1",
+    "Servicio 2",
+    "Servicio 3"
+  ],
+  socialMedia: {
+    facebook: "URL de Facebook",
+    twitter: "URL de Twitter",
+    instagram: "URL de Instagram"
+  },
+  logo: "URL del logo de la empresa",
+  openingHours: {
+    monday: "9:00 AM - 5:00 PM",
+    tuesday: "9:00 AM - 5:00 PM",
+    wednesday: "9:00 AM - 5:00 PM",
+    thursday: "9:00 AM - 5:00 PM",
+    friday: "9:00 AM - 5:00 PM",
+    saturday: "10:00 AM - 4:00 PM",
+    sunday: "Cerrado"
+  }
+};
+
+export default clientData;
