eb105acdcc9bcb9ae6cf3a8388e6646fdefe7bb0
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-08-14 18:56:14 -0300

    Update texts.js

--- src/data/texts.js
+++ src/data/texts.js
@@ -1,1 +1,7 @@
-Hgy
+const texts = {
+  welcomeMessage: "Bienvenidos a",
+  title: "Su Dulce Gracia",
+  description: "Donde cada bocado es una delicia. Desde [año de fundación], nos dedicamos a crear pasteles y postres que no solo deleitan el paladar, sino que también tocan el corazón. Nuestra pasión por la repostería se refleja en cada una de nuestras creaciones."
+};
+
+export default texts;
