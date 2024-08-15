98fb4760e7d448b9e0a519fca9e77b8b3ff458d8
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2024-08-15 19:06:43 -0300

    Update texts.js

--- src/data/texts.js
+++ src/data/texts.js
@@ -1,7 +1,35 @@
+// src/data/texts.js
+
 const texts = {
+  // Textos del componente Home.jsx
   welcomeMessage: "Bienvenidos a",
   title: "Su Dulce Gracia",
-  description: "Donde cada bocado es una delicia. Desde [año de fundación], nos dedicamos a crear pasteles y postres que no solo deleitan el paladar, sino que también tocan el corazón. Nuestra pasión por la repostería se refleja en cada una de nuestras creaciones."
+  description: "Donde cada bocado es una delicia. Desde [año de fundación], nos dedicamos a crear pasteles y postres que no solo deleitan el paladar, sino que también tocan el corazón. Nuestra pasión por la repostería se refleja en cada una de nuestras creaciones.",
+
+  // Textos del componente Especialidad.jsx
+  especialidadesTitle: "Los más pedidos",
+  especialidadesDescription: "Nuestros clientes adoran nuestros productos estrella como la Tarta de Chocolate, el Cheesecake de Frutos Rojos y nuestros Macarons. Cada uno de estos postres es elaborado con ingredientes de la más alta calidad y un toque de amor que los hace únicos.",
+  
+  especialidadesList: [
+    {
+      name: "Feliz Dia!!",
+      description: "Este plato nacional de Perú vive un momento de mucha fama en la actualidad, luego de destacarse en diferentes ferias gastronómicas realizadas en distintas partes del mundo. El ceviche peruano se prepara con pescado o mariscos crudos, cebolla, chile, jugo de limón y sal. Puede ir acompañado de camote, choclo, cancha serrana o chifles.",
+      precio: "$1200",
+      img: "/suDulceGracia/esp/esp1.webp"
+    },
+    {
+      name: "Paella",
+      description: "La paella es un clásico de España que ha sabido ganarse el aprecio internacional. Originaria de Valencia, debe su nombre a la gigante “paellera” dónde se cocina. Se trata de un buen plato de arroz y mariscos, condimentado con azafrán y hierbas verdes. Como siempre, hay muchas versiones y se hace también con pollo, cerdo o, simplemente, vegetales.",
+      precio: "$740",
+      img: "/suDulceGracia/esp/esp2.webp"
+    },
+    {
+      name: "Chocolate!!",
+      description: "Su exquisitez y practicidad les han permitido a los burritos consagrarse como otra de las comidas más populares del universo. Autóctono de México, contiene algunos de los ingredientes más representativos de su país: una tortilla tradicional, a la que se le agrega un relleno -generalmente carne, pollo y maíz- y luego se la enrolla para formar el famoso burrito.",
+      precio: "$780",
+      img: "/suDulceGracia/esp/esp3.jpg"
+    }
+  ]
 };
 
 export default texts;
